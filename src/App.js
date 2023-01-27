6dfd6e51f6724cda323bc44cefca4e2e0470952d
yuval <91323166+yuuval@users.noreply.github.com>
2023-01-27 14:15:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Für Herr Senften</p>
         <table>
           <thead>
             <tr>
