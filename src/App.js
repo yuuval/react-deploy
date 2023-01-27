20b46e7e81adc38889509608b9031ea838d874d7
yuval <91323166+yuuval@users.noreply.github.com>
2023-01-27 10:02:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,77 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <table>
+          <thead>
+            <tr>
+              <th>Base Url</th>
+              <th>Resource Url</th>
+              <th>Method</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>/gradebook</td>
+              <td>/teacher/register</td>
+              <td>POST</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/teacher/login</td>
+              <td>POST</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/grade/add</td>
+              <td>POST</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/grade/delete/{id}</td>
+              <td>DELETE</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/grade/update/{id}</td>
+              <td>PUT</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/grade</td>
+              <td>GET</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/student/register</td>
+              <td>POST</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/student/login</td>
+              <td>POST</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/subject/add</td>
+              <td>POST</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/subject/delete/{id}</td>
+              <td>DELETE</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/subject/update/{id}</td>
+              <td>PUT</td>
+            </tr>
+            <tr>
+              <td>/gradebook</td>
+              <td>/subject</td>
+              <td>GET</td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
