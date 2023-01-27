80c1e9fd4675df543b1318545039d23a31fb4022
yuval <91323166+yuuval@users.noreply.github.com>
2023-01-27 10:00:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Table from './Table';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         <View>
-            <Table />
-         </View>
       </header>
     </div>
   );
